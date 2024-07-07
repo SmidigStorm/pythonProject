b5abf563d5311bef3c7a3b9e73b850aa7ea80375
Arne Henrik Storm <arnehenrik.storm@sikt.no>
2024-07-07 15:22:01 +0200

    Initial Commit2

---
+++ pythonProject/static/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('start-game').addEventListener('click', function() {
+    alert('Game Started!');
+    // Here you can add code to navigate to the game page or initialize game logic
+});
